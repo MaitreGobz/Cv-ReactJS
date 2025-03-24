56e808d4a0304ab97ad83547b88386c4e77db51d
MaitreGobz <nayetlucas@hotmail.fr>
2025-03-24 18:51:52 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react"; 
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return(
+        <header>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark text-light text-uppercase fixed-top" id="mainNav">
+            <div className="container">
+                <a className="navbar-brand text-light" href="#page-top">Nayet Lucas</a>
+                <button className="navbar-toggler collapsed bg-dark " type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item mx-0 mx-lg-1">
+                            <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/accueil">Accueil</Link>
+                        </li>
+                        <li className="nav-item mx-0 mx-lg-1">
+                            <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/services">Services</Link>
+                        </li>
+                        <li className="nav-item mx-0 mx-lg-1">
+                            <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/realisations">Réalisations</Link>
+                        </li>
+                        <li className="nav-item mx-0 mx-lg-1">
+                            <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/blog">Blog</Link>
+                        </li>
+                        <li className="nav-item mx-0 mx-lg-1">
+                            <Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/contact">Me contacter</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+	</header>
+    )
+}
+
+export default Header;
